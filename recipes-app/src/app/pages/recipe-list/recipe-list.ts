9b50c207b3cdbcb86bb7ef925de1ba5af8d911a6
Shevchenko Arina <arina_shevchenko_00@mail.ru>
2026-09-22 02:08:01 +0700

    init: first commit

---
+++ recipes-app/src/app/pages/recipe-list/recipe-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-recipe-list',
+  styleUrl: './recipe-list.css',
+  templateUrl: './recipe-list.html',
+})
+export class RecipeList {}
