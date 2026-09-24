9cf0cf6b9e78b63eef1407732e6db75c7ad3ae39
Shevchenko Arina <arina_shevchenko_00@mail.ru>
2026-09-25 04:47:01 +0700

    clean: remove recipe-list page folder

--- recipes-app/src/app/pages/recipe-list/recipe-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-recipe-list',
-  styleUrl: './recipe-list.css',
-  templateUrl: './recipe-list.html',
-})
-export class RecipeList {}
